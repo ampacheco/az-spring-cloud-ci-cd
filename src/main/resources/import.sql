3eb227c02d7ce5065716bf913cff484a67473d41
Alejandro Pa(c)heco <email@alejandropacheco.net>
2019-11-03 18:40:23 +0000

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO `az_schema`.`weather` (`city`, `description`, `icon`) VALUES ('Paris, France', 'Very cloudy!', 'weather-fog');
+INSERT INTO `az_schema`.`weather` (`city`, `description`, `icon`) VALUES ('London, UK', 'Quite cloudy', 'weather-pouring');
